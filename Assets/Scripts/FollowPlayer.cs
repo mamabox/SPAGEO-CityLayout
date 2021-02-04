6c0967f6180316a2fa9952dbce29a6f3360886b1
Fatou-Maty Diouf <fatou@mamafatou.com>
2021-02-04 08:33:48 +0100

    Initial with changes

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Vector3 cameraOffset = new Vector3(0, 0.75f, 0); //Camera offset from player
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + cameraOffset; // Sets camera to player movement + offset
+        transform.rotation = player.transform.rotation; //Sets camera to player's rotation
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * SIMPLE PLAYER CONTROLLER
+ * 
+ * horizontal input = move
+ * vertical input = rotate
+ * 
+ */
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6.0f; //Player's walking speed
+    public float lookSpeed = 50.0f; //Player's turning speed
+
+    private readonly int xRange = 350; // Ground plane size (x-axis) * 10
+    private readonly int yRange = 350; // Ground plane size (y-axis) * 10
+
+    private Camera playerCamera;
+    private Rigidbody playerRb;
+
+    public Vector3 startPosition; //Used to reset to initial position
+    public Vector3 startRotation; //User to reset to initial rotatin
+
+    public Vector3 currentRotation; //Player's current rotation
+
+    private float horizontalInput; //Value of horizontal input
+    private float verticalInput; //Value of vertical input
+
+    public bool tookStep = false;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = Camera.main; //Set playerCamera to camera with 'main'tag
+        playerRb = GetComponent<Rigidbody>();
+
+        //Record start position and rotation
+        startPosition = transform.position;
+        startRotation = transform.eulerAngles;
+
+        currentRotation = startRotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //PLAYER INPUT
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    
+
+        if (verticalInput > 0)
+        {
+            transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+            tookStep = false;
+        }
+        else if (verticalInput < 0 && !tookStep)
+        {
+            tookStep = true;
+            playerRb.AddRelativeForce(Vector3.back * 500, ForceMode.Impulse);
+        }
+
+        currentRotation.y += horizontalInput * Time.deltaTime * lookSpeed;
+        transform.eulerAngles = new Vector3(0, currentRotation.y, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))    //Return to start 
+        {
+            transform.position = startPosition;
+            currentRotation = startRotation;
+            
+            //ADD WAIT until you can move again
+
+        }
+
+        //Limit player's movement to the plane
+        //if (transform.position.x < 0)
+        //{
+        //    transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        //}
+        //if (transform.position.z < 0)
+        //{
+        //    transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        //}
+    }
+}
